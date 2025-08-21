661393a0ae2ddf0c5206f046b1ae71e4541ca5cc
Tobert03 <tobias.72518@protonmail.com>
2025-08-21 15:34:46 +0200

    Update selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -1,5 +1,5 @@
 
-array1 = [34,32,4,75,6,37,5,44,5,68,7,27,55,44,3,25,2,36,56,7,8,9,]
+array1 = [34,32,4,75,6,37,5,44,5,68,7,27,55,44,3,25,2,36,56,7,8,9]
 
 def selection_sort(array):
     for i in range(len(array)):
@@ -11,5 +11,6 @@ def selection_sort(array):
                 sorted = False
         array.insert(i, lowest)
         print(array)
+
 
 selection_sort(array1)
