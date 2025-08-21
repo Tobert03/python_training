2412c4d827e3e43fc8b9efa69929d77ca344fd4c
Tobert03 <tobias.72518@protonmail.com>
2025-08-21 22:19:51 +0200

    Update selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -1,16 +1,20 @@
 
 array1 = [34,32,4,75,6,37,5,44,5,68,7,27,55,44,3,25,2,36,56,7,8,9]
 
-def selection_sort(array):
-    for i in range(len(array)):
-        sorted = True
-        lowest = array[i]
-        for j in range(i, len(array)):
-            if array[j] < lowest:
-                lowest = array[j]
-                sorted = False
-        array.insert(i, lowest)
-        print(array)
 
+#selection sort bedeutet, dass immer der niedrigste Wert, nach ganz vorne gestellt wird, so lange bis die liste sortiert ist
+def selection_sort(array):
+    for i in range(len(array)):     #die Äußere loop macht so viele Iterationen wie es Elemente zum sortieren gibt
+        lowest = array[i]
+        lowest_ind = 0
+        for j in range(i, len(array)):      #die innere loop startet beim Index i, da ab dort immer die unsortierte Liste anfängt
+            if array[j] < lowest:           #und findet den kleinsten wert, in der noch unsortierten liste
+                lowest = array[j]
+                lowest_ind = j
+        print(array)
+        array.insert(i, lowest)     #der kleinste Wert wird vorne eingefügt 
+        array.pop(lowest_ind+1)     #und an seiner originalen stelle gelöscht
+        
 
 selection_sort(array1)
+
