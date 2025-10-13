bf2edbb7e46a6ed1e14fc43f845ad63c87f2b45f
Tobert03 <tobias.72518@protonmail.com>
2025-10-13 22:49:57 +0200

    added subtraction

--- euclidean_algo.py
+++ euclidean_algo.py
@@ -1,6 +1,6 @@
 
 #the euclidean Algorithm finds the greatest common divisor (gcd)
-def euclidean_algo(a, b):
+def euclidean_algo_divivsion(a, b):
   gcd = 0
   num1 = a
   num2 = b
@@ -11,3 +11,14 @@ def euclidean_algo(a, b):
     else:
       num1,num2 = num2,rest
   return gcd
+
+def eucidean_algo_subtraction(a, b):
+  num1 = max(a, b)
+  num2 = min(a, b)
+  while num1 != num2:
+    if num1 > num2:
+      rest = num1 - num2
+    else:
+      rest = num2 - num1
+    num1, num2 = num2, rest
+  return num1
